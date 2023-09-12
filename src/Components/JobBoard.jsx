808d1b8f131d9bcb879c6d43b63e5fd5e84f07d1
iceror <ilsecervor@gmail.com>
2023-09-11 23:24:34 -0600

    create elements for job board

--- src/Components/JobBoard.jsx
+++ src/Components/JobBoard.jsx
@@ -2,13 +2,31 @@ import JobList from "./JobList"
 import NavBar from "./NavBar"
 
 
-const JobBoard = ({data}) => {
+const JobBoard = ({ data }) => {
 
   return (
     <>
       <NavBar />
-      <h2>Job Board</h2>
-      <JobList data={data} />
+      <main>
+        <div className="filters">
+          <h3>Search by filters</h3>
+          <input type="text" placeholder="Functional Area" />
+          <input type="text" placeholder="Seniority" />
+          <input type="text" placeholder="Perks" />
+          <input type="text" placeholder="Location" />
+        </div>
+        <hr />
+        <div className="search">
+          <h3>Search by keywords</h3>
+          <input type="text" placeholder="Keywords" />
+          <div className="order-by">
+            <h3>Order by:</h3>
+            <button>Recent</button>
+            <button>Companies A-Z</button>
+          </div>
+        </div>
+        <JobList data={data} />
+      </main>
     </>
   )
 }
