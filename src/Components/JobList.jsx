01cb898573781498e9410dc8b7a26cb7dcc75ff4
iceror <ilsecervor@gmail.com>
2023-09-11 22:08:01 -0600

    create ul and render each job in li

--- src/Components/JobList.jsx
+++ src/Components/JobList.jsx
@@ -2,16 +2,35 @@ import { useEffect } from 'react';
 import { getData } from '../js/data-utils';
 import csv from '../assets/data/job-data.csv'
 
-const data = getData()
-console.log(data);
-
 const JobList = () => {
-  console.log(csv);
-
+  const data = getData()
+  console.log(data);
   return (
-    <>
-    <h2>job data here</h2>
-    </>
+    <ul>
+      {data.map((job) => {
+        return (
+          <li>
+            <div className='main-job-info'>
+              <h2>{job['Job Title']}</h2>
+              <p>{job['Company Name']}</p>
+              {/* TODO function that counts days */}
+              <p>Posted x days ago</p>
+            </div>
+            <div className="job-modality">
+              <ul>
+                <li>{job['Job Type']}</li>
+                <li>{job['Location Full']}</li>
+                <li>{job['Seniority']}</li>
+              </ul>
+            </div>
+            <div className="perks">
+              {/* TO DO add perks maybe as an array and add images */}
+            </div>
+            <hr />
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
