6d749fc4b5060eae689518ca7485dbc295d0e64d
iceror <ilsecervor@gmail.com>
2023-09-12 18:14:18 -0600

    create filter for each search filter

--- src/Components/JobBoard.jsx
+++ src/Components/JobBoard.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react"
 import JobList from "./JobList"
 import NavBar from "./NavBar"
 
 
 const JobBoard = ({ data }) => {
+  const [jobs, setJobs] = useState([]);
+  const [functionalArea, setFunctionalArea] = useState('');
+  const [seniority, setSeniority] = useState('');
+  const [perks, setPerks] = useState('');
+  const [location, setLocation] = useState('');
+
+  const handleFunctionAreaChange = (event) => {
+    setFunctionalArea(event.target.value)
+  }
+
+  const handleSeniorityInputChange = (event) => {
+    setSeniority(event.target.value)
+  }
+
+  const handlePerksInputChange = (event) => {
+    setPerks(event.target.value)
+  }
+
+  const handleLocationInputChange = (event) => {
+    setLocation(event.target.value)
+  }
+
+  const filtered = data.filter(job => {
+    return (
+      (functionalArea === '' || job['Functional Area'].toLowerCase().includes(functionalArea.toLowerCase())) &&
+      (seniority === '' || job['Seniority'].toLowerCase().includes(seniority.toLowerCase())) &&
+      (perks === '' || job['Perks (coming soon)'].toLowerCase().includes(perks.toLowerCase())) &&
+      (location === '' || job['Location Full'].toLowerCase().includes(location.toLowerCase()))
+    );
+  });
+
+  useEffect(() => {
+    setJobs(filtered)
+  }, [functionalArea, seniority, perks, location])
+  // console.log(jobs);
 
   return (
     <>
@@ -11,10 +47,10 @@ const JobBoard = ({ data }) => {
         <div className="filters">
           <h3>Search by filters</h3>
           <div>
-            <input type="text" placeholder="Functional Area" />
-            <input type="text" placeholder="Seniority" />
-            <input type="text" placeholder="Perks" />
-            <input type="text" placeholder="Location" />
+            <input type="text" placeholder="Functional Area" onChange={handleFunctionAreaChange} />
+            <input type="text" placeholder="Seniority" onChange={handleSeniorityInputChange} />
+            <input type="text" placeholder="Perks" onChange={handlePerksInputChange} />
+            <input type="text" placeholder="Location" onChange={handleLocationInputChange} />
           </div>
         </div>
         <hr />
@@ -29,7 +65,7 @@ const JobBoard = ({ data }) => {
             <button>Companies A-Z</button>
           </div>
         </div>
-        <JobList data={data} />
+        <JobList data={jobs} />
       </main>
     </>
   )
