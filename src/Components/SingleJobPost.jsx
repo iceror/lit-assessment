b8fccd6def49121cb71d3489af5d46c00e068a9a
iceror <ilsecervor@gmail.com>
2023-09-12 21:55:54 -0600

    create job details section

--- src/Components/SingleJobPost.jsx
+++ src/Components/SingleJobPost.jsx
@@ -1,13 +1,35 @@
 import { useParams } from "react-router";
 
-const SingleJobPost = ({data}) => {
+const SingleJobPost = ({data, getDaysSinceJobPost}) => {
   const { jobPath } = useParams();
   const pathNums = jobPath.match(/\d+/g);
   const jobIndex = Number(pathNums) - 1
-  console.log(jobIndex);
-  console.log(data);
+  const singleJob = data[jobIndex]
+  // console.log(jobIndex);
+  console.log(singleJob);
 
+  return (
+    <section className="job-details">
+      <div className="job-title">
+        <button className="back-to-job-board">Back to Job Board</button>
+        <h2 >{singleJob['Job Title']}</h2>
+      </div>
+      <div className="company">
+        <h2>{singleJob['Company Name']}</h2>
+        {/* <p>{singleJob['Company tagline']}</p> */}
+      </div>
+      <div className="perks">
 
+      </div>
+      <div className="single-job-modality">
+        <p>{singleJob['Job Type']}</p>
+        <p>{singleJob['Location Full'].split(';')}</p>
+        <p>{singleJob['Seniority']}</p>
+        <p>Posted {getDaysSinceJobPost(singleJob['Date Published'])} days ago</p>
+      </div>
+      <button className="apply">Apply Now</button>
+    </section>
+  )
 }
 
 export default SingleJobPost
